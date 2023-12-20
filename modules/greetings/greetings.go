21948063810afa40c91332c1361ec3b46777a295
Tanmay <tl@zasmlabs.com>
2023-12-20 16:45:09 +0530

    added a test

--- modules/greetings/greetings.go
+++ modules/greetings/greetings.go
@@ -3,11 +3,35 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("Name not provided")
 	}
-	return fmt.Sprintf("hello, %v. Welcome!", name), nil
+	return fmt.Sprintf(randomFormat(), name), nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
